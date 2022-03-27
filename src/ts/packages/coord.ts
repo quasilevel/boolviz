4d4e402b754170af224954c78ca845336521e9ba
keogami <41939011+keogami@users.noreply.github.com>
2022-03-27 18:00:23 +0530

    implemented add and scale on coord, and respective mutation alts

--- src/ts/packages/coord.ts
+++ src/ts/packages/coord.ts
@@ -1,7 +1,29 @@
 export default class Coord {
   x: number = 0
   y: number = 0
+
   constructor(x: number, y: number) {
     this.x = x, this.y = y
+  }
+
+  scale(val: number): Coord {
+    return new Coord(
+      this.x * val, this.y * val
+    )
+  }
+
+  add(c: Coord): Coord {
+    return new Coord(
+      this.x + c.x, this.y + c.y
+    )
+  }
+
+  mutScale(val: number) {
+    this.x *= val
+    this.y *= val
+  }
+
+  mutAdd(x: number, y: number) {
+    this.x += x, this.y += y
   }
 }
