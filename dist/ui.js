555fb0c65e8cb6b006df90f1d7292720897edc2c
keogami <41939011+keogami@users.noreply.github.com>
2022-04-01 00:46:38 +0530

    gates can now be added through the pallete

--- dist/ui.js
+++ dist/ui.js
@@ -1,2 +1,37 @@
-"use strict";
-console.log("ui loaded");
+import { requestGateAddition } from "./boolviz.js";
+import { GateType } from "./packages/gates.js";
+const $ = document;
+const gateEnumMap = new Map([
+    ["in", GateType.IN_TERM],
+    ["out", GateType.OUT_TERM],
+    ["not", GateType.NOT],
+    ["and", GateType.AND],
+    ["or", GateType.OR],
+    ["xor", GateType.XOR],
+    ["nand", GateType.NAND],
+    ["nor", GateType.NOR],
+]);
+var GateButtonState;
+(function (GateButtonState) {
+    GateButtonState["SELECTED"] = "selected";
+    GateButtonState["NORMAL"] = "normal";
+})(GateButtonState || (GateButtonState = {}));
+const gatesButtons = $.querySelectorAll(".gate-con");
+gatesButtons.forEach(it => {
+    const el = it;
+    el.dataset.state = GateButtonState.NORMAL;
+    const gate = el.dataset.type;
+    if (typeof gate === "undefined") {
+        console.warn("There exists a .gate-con without a data-type attrib set on it. (skipping)", el);
+        return;
+    }
+    const gt = gateEnumMap.get(gate);
+    el.addEventListener("click", () => {
+        if (el.dataset.state === GateButtonState.SELECTED) {
+            return;
+        }
+        el.dataset.state = GateButtonState.SELECTED;
+        requestGateAddition(gt);
+        console.log(gt);
+    });
+});
--- src/ts/ui.ts
+++ src/ts/ui.ts
@@ -1,1 +1,42 @@
-console.log("ui loaded")
+import { requestGateAddition } from "./boolviz.js"
+import { GateType } from "./packages/gates.js"
+
+const $ = document
+const gateEnumMap = new Map([
+  ["in", GateType.IN_TERM],
+  ["out", GateType.OUT_TERM],
+  ["not", GateType.NOT],
+  ["and", GateType.AND],
+  ["or", GateType.OR],
+  ["xor", GateType.XOR],
+  ["nand", GateType.NAND],
+  ["nor", GateType.NOR],
+])
+
+enum GateButtonState {
+  SELECTED = "selected",
+  NORMAL = "normal"
+}
+
+const gatesButtons = $.querySelectorAll(".gate-con")
+
+gatesButtons.forEach(it => {
+  const el = it as HTMLDivElement
+  el.dataset.state = GateButtonState.NORMAL
+  const gate = el.dataset.type
+  if (typeof gate === "undefined") {
+    console.warn("There exists a .gate-con without a data-type attrib set on it. (skipping)", el)
+    return
+  }
+  const gt = gateEnumMap.get(gate as string)
+
+  el.addEventListener("click", () => {
+    if (el.dataset.state === GateButtonState.SELECTED) {
+      return
+    }
+    el.dataset.state = GateButtonState.SELECTED
+    requestGateAddition(gt as GateType)
+
+    console.log(gt)
+  })
+})
